4545e8853deb5dc4b87b1044942c7811fd888970
ZeeLog <Danjel.Zammit@gmail.com>
2021-01-03 22:53:10 +0100

    mido: Restore Thermal HAL 1.0
    * also, set bitness while we're at it
    
    Revert "mido: Add sepolicy for qti thermal"
    
    This reverts commit f5a19c629470bc99be43705d086a72bf89552961.
    
    Revert "mido: Build QTI thermal 2.0 hal"
    
    This reverts commit 93eb10ab7dd3f7f1a914e51b13db29858497d919.

--- device.mk
+++ device.mk
@@ -363,7 +363,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@1.0-impl:64 \
+    android.hardware.thermal@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
